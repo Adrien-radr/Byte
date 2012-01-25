a557d44eaffe7ac7f0d7c50182cc889ddfb3c93f
revk <greunne@gmail.com>
2012-01-24 19:43:45 -0500

    0.0.11 :
    - Added Font Atlas creation and utilisation in rendering. Phew! Lots of
    troubles on that one. Next step : create functions and structures for
    text/fonts. Everything is still in main.c :p!

--- src/main.c
+++ src/main.c
@@ -13,6 +13,16 @@
 
 FT_Face face;
 
+int fw, fh;
+typedef struct {
+    vec2    advance,
+            size,
+            position;
+
+    f32     x_offset;
+} Glyph;
+
+
 void DrawText( u32 vbo, u32 shader, u32 tex, const char *text, f32 posX, f32 posY ) {
     Color c = { .r = 1, .g = 0, .b = 0, .a = 1 };
     Renderer_useShader( shader );
@@ -82,6 +92,72 @@ void DrawText( u32 vbo, u32 shader, u32 tex, const char *text, f32 posX, f32 pos
         x += (g->advance.x >> 6) * sx;
         y += (g->advance.y >> 6) * sy;
     }
+}
+
+Glyph glyphs[128];
+
+void RenderText( u32 vbo, u32 shader, u32 tex, const char *text, f32 posX, f32 posY ) {
+    // 6 vertices of 4 float(pos<x,y>, texCoord<s,t>), for each character
+    const size_t c_size = 6 * 4;
+    const size_t text_len = strlen(text);
+    const size_t text_data_size = c_size * text_len;
+    f32 data[text_data_size];    
+
+    const Color col = { .r = 1, .g = 0, .b = 0, .a = 1 };
+    Renderer_useShader( shader );
+    Shader_sendColor( "Color", &col );
+
+    glBindBuffer( GL_ARRAY_BUFFER, vbo );
+    glEnableVertexAttribArray( 0 );
+    glEnableVertexAttribArray( 1 );
+    glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, 0, 0 );
+    glVertexAttribPointer( 1, 2, GL_FLOAT, GL_FALSE, 0, (void*)(sizeof(f32)*text_data_size/2) );
+
+    Renderer_useTexture( tex, 0 );
+
+
+    f32 x = posX, y = posY;
+
+    vec2 ws = Context_getSize();
+    f32 sx = 2.f / ws.x, sy = 2.f / ws.y;
+
+    int n_pos = 0, n_tex = text_data_size/2;
+
+    for( const char *p = text; *p; ++p ) {
+        int i = (int)*p;
+        f32 x2 = x  + glyphs[i].position.x * sx;
+        f32 y2 = -y - glyphs[i].position.y * sy;
+        f32 w  = glyphs[i].size.x * sx;
+        f32 h  = glyphs[i].size.y * sy;
+
+        if( !w || !h ) continue;
+
+        x += glyphs[i].advance.x * sx;
+        y += glyphs[i].advance.y * sy;
+
+        // positions triangle 1
+        data[n_pos++] = x2;         data[n_pos++] = -y2;
+        data[n_pos++] = x2;         data[n_pos++] = -y2 - h;
+        data[n_pos++] = x2 + w;     data[n_pos++] = -y2 - h;
+
+        // positions triangle 2
+        data[n_pos++] = x2;         data[n_pos++] = -y2;
+        data[n_pos++] = x2 + w;     data[n_pos++] = -y2 - h;
+        data[n_pos++] = x2 + w;     data[n_pos++] = -y2;
+
+        // texcoords triangle 1
+        data[n_tex++] = glyphs[i].x_offset;                                         data[n_tex++] = 0;
+        data[n_tex++] = glyphs[i].x_offset;                                         data[n_tex++] = (f32)glyphs[i].size.y / (f32)fh;
+        data[n_tex++] = glyphs[i].x_offset + (f32)glyphs[i].size.x / (f32)fw;       data[n_tex++] = (f32)glyphs[i].size.y / (f32)fh;
+
+        // texcoords triangle 2
+        data[n_tex++] = glyphs[i].x_offset;                                         data[n_tex++] = 0;
+        data[n_tex++] = glyphs[i].x_offset + (f32)glyphs[i].size.x / (f32)fw;       data[n_tex++] = (f32)glyphs[i].size.y / (f32)fh;
+        data[n_tex++] = glyphs[i].x_offset + (f32)glyphs[i].size.x / (f32)fw;       data[n_tex++] = 0;
+    }
+
+    glBufferData( GL_ARRAY_BUFFER, sizeof( data ), data, GL_DYNAMIC_DRAW );
+    glDrawArrays( GL_TRIANGLES, 0, text_len * 6 );
 }
 
 int main() {
@@ -181,57 +257,6 @@ int main() {
     int textShader = World_loadResource( world, RT_Shader, "textShader.json" );
     check( textShader >= 0, "Error in text shader creation. Exiting!\n" );
 
-    FT_Library *ft = Device_getFreetype();
-
-    check( !FT_New_Face( *ft, "/usr/share/fonts/TTF/LiberationMono-Regular.ttf", 0, &face ), "Could not open font visitor!\n" );
-
-
-    FT_Set_Pixel_Sizes( face, 0, 16 );
-/*
-    FT_GlyphSlot g = face->glyph;
-
-    int w = 0, h = 0;
-
-    for( u32 i = 32; i < 128; ++i ) {
-        check( !FT_Load_Char( face, i, FT_LOAD_RENDER ), "Could not load char %d : %c\n", i, (char)i );
-
-        w += g->bitmap.width;
-        h = max( h, g->bitmap.rows );
-    }
-
-    log_info( "w = %d, h = %d\n", w, h );
-
-
-    
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
-
-    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RED, GL_UNSIGNED_BYTE, 0 );
-CheckGLError();
-    int x = 0;
-
-    for( u32 i = 32; i < 128; ++i ) {
-        if( FT_Load_Char( face, i, FT_LOAD_RENDER ) )
-            continue;
-
-        printf( "%d\n", g->bitmap.rows );
-        glTexSubImage2D( GL_TEXTURE_2D, 0, x, 0, g->bitmap.width, 
-                                                 g->bitmap.rows,
-                                                 GL_RED, 
-                                                 GL_UNSIGNED_BYTE,
-                                                 g->bitmap.buffer );
-
-CheckGLError();
-        x += g->bitmap.width;
-    }
-
-    glPixelStorei( GL_UNPACK_ALIGNMENT, 4 );
-
-
-*/
-
 
     u32 tex;
     glActiveTexture(GL_TEXTURE0);
@@ -239,77 +264,71 @@ CheckGLError();
     glBindTexture( GL_TEXTURE_2D, tex );
     glPixelStorei( GL_UNPACK_ALIGNMENT, 1 );
 
+    
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
+
+
+    FT_Library *ft = Device_getFreetype();
+
+    check( !FT_New_Face( *ft, "/usr/share/fonts/TTF/LiberationMono-Regular.ttf", 0, &face ), "Could not open font visitor!\n" );
+
+
+    FT_Set_Pixel_Sizes( face, 0, 16 );
+
+    // create atlas for font
+        FT_GlyphSlot g = face->glyph;
+
+
+        for( u32 i = 32; i < 128; ++i ) {
+            check( !FT_Load_Char( face, i, FT_LOAD_RENDER ), "Could not load char %d : %c\n", i, (char)i );
+
+            fw += g->bitmap.width;
+            fh = max( fh, g->bitmap.rows );
+        }
+
+
+
+        glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, fw, fh, 0, GL_RED, GL_UNSIGNED_BYTE, 0 );
+        int x = 0;
+
+        for( u32 i = 32; i < 128; ++i ) {
+            if( FT_Load_Char( face, i, FT_LOAD_RENDER ) )
+                continue;
+
+            glTexSubImage2D( GL_TEXTURE_2D, 0, x, 0, g->bitmap.width, 
+                                                     g->bitmap.rows,
+                                                     GL_RED, 
+                                                     GL_UNSIGNED_BYTE,
+                                                     g->bitmap.buffer );
+
+            glyphs[i].advance.x = g->advance.x >> 6;
+            glyphs[i].advance.y = g->advance.y >> 6;
+
+            glyphs[i].size.x = g->bitmap.width;
+            glyphs[i].size.y = g->bitmap.rows;
+
+            glyphs[i].position.x = g->bitmap_left;
+            glyphs[i].position.y = g->bitmap_top;
+
+            glyphs[i].x_offset = (f32)x / (f32)fw;
+
+            x += g->bitmap.width;
+        }
+
+        glPixelStorei( GL_UNPACK_ALIGNMENT, 4 );
+
+
+
+
 
 
     u32 textVbo;
     glGenBuffers( 1, &textVbo );
     glBindBuffer( GL_ARRAY_BUFFER, textVbo );
 
-    /*
-    Color c = { .r = 1, .g = 0, .b = 0, .a = 1 };
-    Renderer_useShader( textShader );
-    Shader_sendColor( "Color", &c );
-
-    glEnableVertexAttribArray( 0 );
-    glEnableVertexAttribArray( 1 );
-    glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, 0, 0 );
-    glVertexAttribPointer( 1, 2, GL_FLOAT, GL_FALSE, 0, (void*)(4 * 2 * sizeof( f32 )) );
-
-    Renderer_useTexture( tex, 0 );
-
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
-
-    glPixelStorei( GL_UNPACK_ALIGNMENT, 1 );
-
-    const char text[] = "H";
-    const char *p = text;
-    vec2 ws = Context_getSize();
-
-    f32 sx = 2.f / ws.x;
-    f32 sy = 2.f / ws.y;
-
-    f32 x = 0, y = 0;
-
-        if( FT_Load_Char( face, *p, FT_LOAD_RENDER ) ) 
-            return -1;
-
-        FT_GlyphSlot g = face->glyph;
-
-
-        glTexImage2D(   GL_TEXTURE_2D, 
-                        0, 
-                        GL_RGBA, 
-                        g->bitmap.width,
-                        g->bitmap.rows,
-                        0,
-                        GL_RGBA,
-                        GL_UNSIGNED_BYTE,
-                        g->bitmap.buffer );
-
-    CheckGLError();
-        f32 x2 = x + g->bitmap_left * sx;
-        f32 y2 = -y - g->bitmap_top * sy;
-        f32 w = g->bitmap.width * sx;
-        f32 h = g->bitmap.rows * sy;
-
-        float har[] = {
-            x2,     -y2 - h,
-            x2 + w, -y2 - h,
-            x2,     -y2,
-            x2 + w, -y2,
-
-            0, 1,
-            1, 1,
-            0, 0,
-            1, 0,
-        };
-
-        glBufferData( GL_ARRAY_BUFFER, sizeof( har ), har, GL_DYNAMIC_DRAW );
-
-*/
 
 
     ////////////////////////////////////
@@ -345,8 +364,9 @@ CheckGLError();
             vec2 ws = Context_getSize();
             f32 sx = 2.f / ws.x;
             f32 sy = 2.f / ws.y;
-//        glDrawArrays( GL_TRIANGLE_STRIP, 0, 4 );
-            DrawText( textVbo, textShader, tex, "1234567890-=_+\\|}][{'\";:/?.>,<mM!@#$%^&**()HThe quick brown fox jumps over the lazy dog", -1*400*sx, -1*-288*sy );
+            //DrawText( textVbo, textShader, tex, "1234567890-=_+\\|}][{'\";:/?.>,<mM!@#$%^&**()HThe quick brown fox jumps over the lazy dog", -1*400*sx, -1*-288*sy );
+            RenderText( textVbo, textShader, tex,  "Hello", -1 *400*sx, -1*-288*sy );
+            //RenderText( textVbo, textShader, tex,  "1234567890-=_+\\|}][{'\";:/?.>,<mM!@#$%^&**()HThe quick brown fox jumps over the lazy dog", -1 *400*sx, -1*-288*sy );
         Device_endFrame();
     }
 
