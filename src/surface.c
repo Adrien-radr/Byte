a3f448f1f120ec1bb50b5a2620a922167099f68c
Matthias Dubouchet <beulard@gmail.com>
2012-03-27 18:53:31 +0200

    1.0.1 :
    + Deleted useless files.

--- src/surface.c
+++
@@ -1,7 +0,0 @@
-#include "surface.h"
-
-Surface* Surface_loadTexture(const char* file){
-    Surface* s = byte_alloc(sizeof(Surface));
-    s->mTexture = Renderer_createTexture(file, true);
-    return s;
-}
